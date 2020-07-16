99694a749af55e3d8a49a29fe2c49660bcaa3028
Paul Joshi <49583322+PaulJoshi@users.noreply.github.com>
2020-07-16 19:50:08 +0530

    Updates

---
+++ precache-manifest.5b6757341f90fec0035b9ad5b6c35b2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bcf69d2a83e8d66f4114046805596c3",
+    "url": "/showatch/index.html"
+  },
+  {
+    "revision": "e17b0374e45a45363069",
+    "url": "/showatch/static/js/2.058881a1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/showatch/static/js/2.058881a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f2fe6cbb3ffe81db2ee",
+    "url": "/showatch/static/js/main.63134542.chunk.js"
+  },
+  {
+    "revision": "6f61f8ae1615aa62f581",
+    "url": "/showatch/static/js/runtime-main.4bb72f55.js"
+  }
+]);
